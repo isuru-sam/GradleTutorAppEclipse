8e282254ed08096264ad9d6f7e909de93f86a849
isamaraweera <isamaraweera@accelaero.com>
2018-06-18 14:53:43 +0530

    test

--- TutorService/src/test/java/LibraryTest.java
+++ TutorService/src/test/java/LibraryTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.*;
 
 public class LibraryTest {
     @Test public void testSomeLibraryMethod() {
-        Library classUnderTest = new Library();
+        com.tutorapp.Library classUnderTest = new com.tutorapp.Library();
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
     }
 }
